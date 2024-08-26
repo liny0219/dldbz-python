3a785581929016b0ebdd7d4218015521ce532c7c
vee <liny0219@gmail.com>
2024-08-27 01:01:58 +0800

    feat: 增加resource_path方法，以便打包后能正确识别资源路径

--- engine/comparator.py
+++ engine/comparator.py
@@ -1,17 +1,16 @@
+import os
+import sys
 import cv2
 import numpy as np
 import utils.loger as loger
 from utils.config_loader import cfg_common
-from utils.image_process import get_pixel_color ,check_image_similarity, find_target_in_image,\
+from utils.image_process import get_pixel_color, check_image_similarity, find_target_in_image, \
     compute_mask, color_match_all, color_match_count, color_in_image
 from utils.status import MATCH_CONFIDENCE
-from paddleocr import paddleocr,PaddleOCR, draw_ocr
-import logging
-#关闭paddleocr的debug信息
-paddleocr.logging.disable(logging.DEBUG)
+
 
 class Comparator:
-    def __init__(self, controller = None):
+    def __init__(self, controller=None):
         """
         Initializes a Comparator object.
 
@@ -22,11 +21,11 @@ class Comparator:
         - None
         """
         self.controller = controller
-        self.ocr = PaddleOCR(lang='ch')
-        self.match_thresholds =  cfg_common.get("comparator.pic_match_similaritys")
-        self.levels = {MATCH_CONFIDENCE.HIGH:0, MATCH_CONFIDENCE.MID:1, MATCH_CONFIDENCE.LOW:2, MATCH_CONFIDENCE.VERY_LOW:3}
-    
-    def _template_image(self, template_path, convert_gray = True):
+        self.match_thresholds = cfg_common.get("comparator.pic_match_similaritys")
+        self.levels = {MATCH_CONFIDENCE.HIGH: 0, MATCH_CONFIDENCE.MID: 1,
+                       MATCH_CONFIDENCE.LOW: 2, MATCH_CONFIDENCE.VERY_LOW: 3}
+
+    def _template_image(self, template_path, convert_gray=True):
         '''
         获取模板图像.
 
@@ -44,8 +43,8 @@ class Comparator:
             return gray_img
         else:
             return color_img
-    
-    def _cropped_screenshot(self, leftup_coordinate = None, rightdown_coordinate = None, convert_gray=True, save_path=''):
+
+    def _cropped_screenshot(self, leftup_coordinate=None, rightdown_coordinate=None, convert_gray=True, save_path=''):
         '''
         获取截屏.
 
@@ -56,9 +55,9 @@ class Comparator:
         '''
 
         color_img = self.controller.capture_screenshot()
-        if(leftup_coordinate and rightdown_coordinate):
+        if (leftup_coordinate and rightdown_coordinate):
             x1, y1 = leftup_coordinate
-            x2, y2 = rightdown_coordinate    
+            x2, y2 = rightdown_coordinate
             color_img = color_img[y1:y2, x1:x2]
 
         if save_path:
@@ -69,7 +68,6 @@ class Comparator:
             return gray_img
         else:
             return color_img
-    
 
     def match_color_all(self, coordinates_colors):
         """
@@ -85,7 +83,6 @@ class Comparator:
         """
         image = self._cropped_screenshot()
         return color_match_all(image, coordinates_colors)
-
 
     def match_color_count(self, coordinates_colors):
         """
@@ -112,10 +109,21 @@ class Comparator:
         - 如果区域内有像素点与指定颜色匹配，则返回 True, 否则返回 False
         """
         cropped_image = self._cropped_screenshot(leftup_coordinate, rightdown_coordinate, convert_gray=False)
-        return  color_in_image(cropped_image, expected_color)
-     
-    def template_in_picture(self, template_path, leftup_coordinate=None, rightdown_coordinate=None, 
-                            return_center_coord = False, save_path = '', match_level = MATCH_CONFIDENCE.HIGH):
+        return color_in_image(cropped_image, expected_color)
+
+    def resource_path(slef, relative_path):
+        """获取资源的绝对路径，处理打包和非打包两种情况"""
+        try:
+            # PyInstaller 创建临时文件夹，保存打包的文件
+            base_path = sys._MEIPASS
+        except Exception:
+            # 未打包时使用的目录
+            base_path = os.path.abspath(".")
+
+        return os.path.join(base_path, relative_path)
+
+    def template_in_picture(self, template_path, leftup_coordinate=None, rightdown_coordinate=None,
+                            return_center_coord=False, save_path='', match_level=MATCH_CONFIDENCE.HIGH):
         '''
         检查指定区域的图像是否存在指定图像模板。
         如果有返回目标坐标
@@ -128,34 +136,34 @@ class Comparator:
         返回：
         - 如果存在指定图像模板，则返回图像模板在指定区域中心点的坐标, 否则返回 None
         '''
-        template_gray = self._template_image(template_path)
+        asset_path = self.resource_path(template_path)
+        template_gray = self._template_image(asset_path)
 
-        cropped_screenshot_gray  = self._cropped_screenshot(leftup_coordinate, rightdown_coordinate, save_path=save_path)
+        cropped_screenshot_gray = self._cropped_screenshot(leftup_coordinate, rightdown_coordinate, save_path=save_path)
 
-        #image_gray中最符合模板template_gray的区域的左上角, 右下角坐标. 且该区域与模板shape一致.
+        # image_gray中最符合模板template_gray的区域的左上角, 右下角坐标. 且该区域与模板shape一致.
         target_leftup, target_rightdown = find_target_in_image(template_gray, cropped_screenshot_gray)
-        #第二次裁剪, 为了匹配模板template_gray的shape, 此时twice_cropped_screenshot_gray与template_gray有相同shape, 这之后才可调用比较相似度的函数
-        twice_cropped_screenshot_gray = cropped_screenshot_gray[target_leftup[1]: target_rightdown[1], target_leftup[0] : target_rightdown[0]]
+        # 第二次裁剪, 为了匹配模板template_gray的shape, 此时twice_cropped_screenshot_gray与template_gray有相同shape, 这之后才可调用比较相似度的函数
+        twice_cropped_screenshot_gray = cropped_screenshot_gray[target_leftup[1]
+            : target_rightdown[1], target_leftup[0]: target_rightdown[0]]
 
         match_threshold = self.match_thresholds[self.levels[match_level]]
 
         # 检查是否匹配
         is_match = check_image_similarity(twice_cropped_screenshot_gray, template_gray, match_threshold)
-        
-               
-        
-        if not return_center_coord: # 如果不需要返回目标中心坐标
+
+        if not return_center_coord:  # 如果不需要返回目标中心坐标
             return is_match
-        else: # 如果需要返回目标中心坐标
-            if not is_match: # 如果不匹配, 说明没找到图像
+        else:  # 如果需要返回目标中心坐标
+            if not is_match:  # 如果不匹配, 说明没找到图像
                 return None
-            else: # 如果匹配
-                if leftup_coordinate: #如果指定了背景图片, 返回全屏的绝对坐标
+            else:  # 如果匹配
+                if leftup_coordinate:  # 如果指定了背景图片, 返回全屏的绝对坐标
                     return get_abs_center_coord(leftup_coordinate, target_leftup, target_rightdown)
-                else:# 如果未指定背景图片, 默认背景图片就是全图, 返回全屏的绝对坐标
-                    return get_abs_center_coord((0,0), target_leftup, target_rightdown)
-                        
-             
+                else:  # 如果未指定背景图片, 默认背景图片就是全图, 返回全屏的绝对坐标
+                    return get_abs_center_coord((0, 0), target_leftup, target_rightdown)
+
+
 def get_abs_center_coord(leftup_coordinate, target_leftup, target_rightdown):
     """
     This function calculates the absolute center coordinates of a target area within a larger image.
@@ -168,7 +176,6 @@ def get_abs_center_coord(leftup_coordinate, target_leftup, target_rightdown):
     Returns:
     tuple: The absolute center coordinates (x, y) of the target area.
     """
-    abs_x_center = int( leftup_coordinate[0] + (target_leftup[0] + target_rightdown[0]) // 2 )
+    abs_x_center = int(leftup_coordinate[0] + (target_leftup[0] + target_rightdown[0]) // 2)
     abs_y_center = int(leftup_coordinate[1] + (target_leftup[1] + target_rightdown[1]) // 2)
-    return (abs_x_center ,abs_y_center)
- 
+    return (abs_x_center, abs_y_center)
