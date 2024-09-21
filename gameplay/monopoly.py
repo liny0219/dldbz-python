52e5ff87348ad9291569bd13bd75d4defad98b7d
vee <liny0219@gmail.com>
2024-09-22 05:26:00 +0800

    fix: 修复bp识别bug

--- gameplay/monopoly.py
+++ gameplay/monopoly.py
@@ -617,6 +617,8 @@ class Monopoly():
             offset = 58 * bp
             end_point = (x, y - offset)
             engine.long_press_and_drag(start_point, end_point)
+            time.sleep(0.5)
+            engine.device.click(x, y)
         if bp == 0:
             engine.device.click(x, y)
         self.update_ui(f"第{roll_time}次投骰子, BP: {bp}")
@@ -985,22 +987,31 @@ class Monopoly():
         bp3 = [865, 456]
         bp2 = [848, 456]
         bp1 = [832, 456]
-        r_bp1 = screenshot[bp1[1], bp1[0]][2]  # 获取 R 通道
-        r_bp2 = screenshot[bp2[1], bp2[0]][2]  # 获取 R 通道
-        r_bp3 = screenshot[bp3[1], bp3[0]][2]  # 获取 R 通道
-        limit = 80
+        pt_bp1 = screenshot[bp1[1], bp1[0]]
+        pt_bp2 = screenshot[bp2[1], bp2[0]]
+        pt_bp3 = screenshot[bp3[1], bp3[0]]
+        b_bp1 = pt_bp1[0]
+        b_bp2 = pt_bp2[0]
+        b_bp3 = pt_bp3[0]
+        g_bp1 = pt_bp1[1]
+        g_bp2 = pt_bp2[1]
+        g_bp3 = pt_bp3[1]
+        r_bp1 = pt_bp1[2]
+        r_bp2 = pt_bp2[2]
+        r_bp3 = pt_bp3[2]
+        limit = 100
         result = 0
-        if r_bp1 > limit:
+        if r_bp1 > limit and g_bp1 > limit and b_bp1 > limit:
             result = 1
-        if r_bp2 > limit:
+        if r_bp2 > limit and g_bp2 > limit and b_bp2 > limit:
             result = 2
-        if r_bp3 > limit:
+        if r_bp3 > limit and g_bp3 > limit and b_bp3 > limit:
             result = 3
 
         print(f"r_bp1:{r_bp1},r_bp2:{r_bp2},r_bp3:{r_bp3}")
-        del r_bp1
-        del r_bp2
-        del r_bp3
+        del pt_bp1
+        del pt_bp2
+        del pt_bp3
         return result
 
     def on_get_enmey(self):
