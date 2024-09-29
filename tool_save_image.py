c9d08903be91513981067907b3a2988ee334efa1
vee <liny0219@gmail.com>
2024-09-29 22:00:15 +0800

    feat: 添加看广告的截图

--- tool_save_image.py
+++ tool_save_image.py
@@ -4,4 +4,4 @@ from engine.comparator import comparator
 u2_device.connect()
 comparator.set_device(u2_device.device)
 
-comparator._cropped_screenshot([64, 397], [126, 415], save_path="./assets/ads/finish.png")
+comparator._cropped_screenshot([51, 425], [136, 440], save_path="./assets/ads/finish_vip.png")
